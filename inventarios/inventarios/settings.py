4ddcc7d513f2693536a9abdbfc66439af9e7639e
Esteban AA <62820991+eathekilla@users.noreply.github.com>
2023-07-19 17:56:36 -0500

    Update settings.py

--- inventarios/inventarios/settings.py
+++ inventarios/inventarios/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c!ln%xnu77e9&vloqlfmi!ldz-7c76g(*hva13(#ba5f61xg*3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 CORS_ALLOW_ALL_ORIGINS = [
     'http://localhost:3000',
