11c94015e2f2bbf59c45984ca8a53585064dc5e1
Esteban AA <62820991+eathekilla@users.noreply.github.com>
2023-08-29 17:43:48 -0500

    FIX(ENTRATAS INSUMOS)

--- inventarios/Entrada/serializers.py
+++ inventarios/Entrada/serializers.py
@@ -1,19 +1,12 @@
 from rest_framework import serializers
 from .models import Entrada
-from Insumo.models import Insumo
 from Insumo.serializers import InsumoSerializer
 
 class EntradaSerializer(serializers.ModelSerializer):
-    insumo = serializers.PrimaryKeyRelatedField(source='insumo', queryset=Insumo.objects.all(), write_only=True)
-
+    
     class Meta:
         model = Entrada
-        fields = ['fecha_creacion', 'fecha_ingreso', 'de_finca', 'a_finca', 'cantidad', 'valor_unitario_entrada_a', 'total_entra_a_la_finca', 'identificador', 'insumo']
-
-    def create(self, validated_data):
-        insumo_id = validated_data.pop('insumo_id')
-        entrada = Entrada.objects.create(insumo_id=insumo_id, **validated_data)
-        return entrada
+        fields = '__all__'
 
 class EntradaSerializerHistorico(serializers.ModelSerializer):
     insumo = InsumoSerializer()
