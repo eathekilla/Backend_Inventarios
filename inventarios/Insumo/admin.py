f266e943b092ffc5125a4acd01e0e8b50431a355
Esteban AA <62820991+eathekilla@users.noreply.github.com>
2023-10-31 18:43:29 -0500

    Update admin.py

--- inventarios/Insumo/admin.py
+++ inventarios/Insumo/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-from .models import Insumo
+from .models import Insumo,IngredienteActivo,Certificacion,UnidadMedida,Grupo
 admin.site.register(Insumo)
+admin.site.register(IngredienteActivo)
+admin.site.register(Certificacion)
+admin.site.register(UnidadMedida)
+admin.site.register(Grupo)
