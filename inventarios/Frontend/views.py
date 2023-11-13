6bf6418164d9a9f12b6e63f2ed6c6f9cd9a0dece
Esteban AA <eamezquita97@gmail.com>
2023-11-13 17:38:09 -0500

    feat(usuarios y roles=

--- inventarios/Frontend/views.py
+++ inventarios/Frontend/views.py
@@ -48,9 +48,10 @@ def test_view(request):
 def add_user(request,user_id=None):
     user = get_object_or_404(User,email=request.user.email)
     token = str(AccessToken.for_user(user))
+    grupos_usuario = list(request.user.groups.values_list('name', flat=True))
     if user_id:
         user_instance = get_object_or_404(User, id=user_id)
-        return render(request,"html/app/add-usuario.html",{'userId': user_instance.pk,'token':token})
+        return render(request,"html/app/add-usuario.html",{'userId': user_instance.pk,'token':token,'grupos_usuario': grupos_usuario})
         
     else:
         return render(request,"html/app/add-usuario.html",{'token':token})
@@ -59,24 +60,27 @@ def add_user(request,user_id=None):
 def list_users(request):
     user = get_object_or_404(User,email=request.user.email)
     token = str(AccessToken.for_user(user))
-    context = {"token":token}
+    grupos_usuario = list(request.user.groups.values_list('name', flat=True))
+    context = {"token":token,'grupos_usuario': grupos_usuario}
     return render(request,"html/app/list-usuarios.html",context) 
 @login_required
 def add_proveedor(request,prov_id=None):
     user = get_object_or_404(User,email=request.user.email)
     token = str(AccessToken.for_user(user))
+    grupos_usuario = list(request.user.groups.values_list('name', flat=True))
     if prov_id:
         prov_instance = get_object_or_404(Proveedor,id=prov_id)
-        return render(request,"html/app/add-proveedor.html",{'providerId': prov_instance.pk,'token':token})
+        return render(request,"html/app/add-proveedor.html",{'providerId': prov_instance.pk,'token':token,'grupos_usuario': grupos_usuario})
     else:
-        return render(request,"html/app/add-proveedor.html",{'token':token})
+        return render(request,"html/app/add-proveedor.html",{'token':token,'grupos_usuario': grupos_usuario})
 @login_required
 def list_proveedor(request):
     user = get_object_or_404(User,email=request.user.email)
     token = str(AccessToken.for_user(user))
     proveedores = Proveedor.objects.all()
+    grupos_usuario = list(request.user.groups.values_list('name', flat=True))
 
-    return render(request,"html/app/list-proveedor.html",{'token':token,'proveedores':proveedores,'user':user}) 
+    return render(request,"html/app/list-proveedor.html",{'token':token,'proveedores':proveedores,'user':user,'grupos_usuario': grupos_usuario}) 
 
 @login_required
 def add_insumo(request,insumo_id=None):
@@ -84,12 +88,14 @@ def add_insumo(request,insumo_id=None):
     certificaciones = Certificacion.objects.all()
     grupo_insumos = Grupo.objects.all()
     user = get_object_or_404(User,email=request.user.email)
+    grupos_usuario = list(request.user.groups.values_list('name', flat=True))
     token = str(AccessToken.for_user(user))
     context ={
         "unidades_medida":unidades_medida,
         "certificaciones":certificaciones
         ,'token':token
         ,"grupos":grupo_insumos
+        ,'grupos_usuario': grupos_usuario
     }
     if insumo_id:
         insumo_instance = get_object_or_404(Insumo,id=insumo_id)
@@ -103,14 +109,16 @@ def list_insumo(request):
     user = get_object_or_404(User,email=request.user.email)
     token = str(AccessToken.for_user(user))
     insumo_instance = Insumo.objects.all()
-    return render(request,"html/app/list-insumos.html",{"insumos":insumo_instance,'token':token})
+    grupos_usuario = list(request.user.groups.values_list('name', flat=True))
+    return render(request,"html/app/list-insumos.html",{"insumos":insumo_instance,'token':token,'grupos_usuario': grupos_usuario})
 
 @login_required
 def add_certificacion(request,cert_id=None):
     user = get_object_or_404(User,email=request.user.email)
     token = str(AccessToken.for_user(user))
     ingredientes = IngredienteActivo.objects.all()
-    context = {"ingredientes":ingredientes,'token':token}
+    grupos_usuario = list(request.user.groups.values_list('name', flat=True))
+    context = {"ingredientes":ingredientes,'token':token,'grupos_usuario': grupos_usuario}
 
     if cert_id:
         cert_instance = get_object_or_404(Certificacion,pk=cert_id)
@@ -125,8 +133,9 @@ def list_certificacion(request):
     certificaciones = Certificacion.objects.all()
     user = get_object_or_404(User,email=request.user.email)
     token = str(AccessToken.for_user(user))
+    grupos_usuario = list(request.user.groups.values_list('name', flat=True))
     
-    return render(request,"html/app/list-certificacion.html",{'certificaciones':certificaciones,'token':token})
+    return render(request,"html/app/list-certificacion.html",{'certificaciones':certificaciones,'token':token,'grupos_usuario': grupos_usuario})
 
 
 @login_required
@@ -134,7 +143,8 @@ def add_ingrediente(request,ingrediente_id=None):
     ingredientes = IngredienteActivo.objects.all()
     user = get_object_or_404(User,email=request.user.email)
     token = str(AccessToken.for_user(user))
-    context = {'ingredientes': ingredientes,'token':token}
+    grupos_usuario = list(request.user.groups.values_list('name', flat=True))
+    context = {'ingredientes': ingredientes,'token':token,'grupos_usuario': grupos_usuario}
     if ingrediente_id:
         insumo_instance = get_object_or_404(IngredienteActivo,pk=ingrediente_id)
         context['ingrediente_id'] = insumo_instance.pk
@@ -149,7 +159,8 @@ def list_ingrediente(request):
     user = get_object_or_404(User,email=request.user.email)
     token = str(AccessToken.for_user(user))
     ingredientes = IngredienteActivo.objects.all()
-    context = {'ingredientes': ingredientes,'token':token}
+    grupos_usuario = list(request.user.groups.values_list('name', flat=True))
+    context = {'ingredientes': ingredientes,'token':token,'grupos_usuario': grupos_usuario}
     return render(request,"html/app/list-ingrediente.html",context)
 
 
@@ -158,7 +169,8 @@ def add_grupo(request,grupo_id=None):
     insumos = Insumo.objects.all()
     user = get_object_or_404(User,email=request.user.email)
     token = str(AccessToken.for_user(user))
-    context = {"insumos":insumos,'token':token}
+    grupos_usuario = list(request.user.groups.values_list('name', flat=True))
+    context = {"insumos":insumos,'token':token,'grupos_usuario': grupos_usuario}
     
     if grupo_id:
         grupo_instance = get_object_or_404(Grupo,pk=grupo_id)
@@ -174,18 +186,20 @@ def list_grupo(request):
     user = get_object_or_404(User,email=request.user.email)
     token = str(AccessToken.for_user(user))
     grupos = Grupo.objects.all()
-    context = {"grupos":grupos,'token':token}
+    grupos_usuario = list(request.user.groups.values_list('name', flat=True))
+    context = {"grupos":grupos,'token':token,'grupos_usuario': grupos_usuario}
     return render(request,"html/app/list-grupos.html",context)
 
 @login_required
 def add_unidad(request,unidad_id=None):
     user = get_object_or_404(User,email=request.user.email)
     token = str(AccessToken.for_user(user))
+    grupos_usuario = list(request.user.groups.values_list('name', flat=True))
     if unidad_id:
         insumo_instance = get_object_or_404(UnidadMedida,pk=unidad_id)
-        return render(request,"html/app/add-unidad.html",{'unidad_id': insumo_instance.pk,'token':token})
+        return render(request,"html/app/add-unidad.html",{'unidad_id': insumo_instance.pk,'token':token,'grupos_usuario': grupos_usuario})
     else:
-        return render(request,"html/app/add-unidad.html",{'token':token})
+        return render(request,"html/app/add-unidad.html",{'token':token,'grupos_usuario': grupos_usuario})
 
 
 @login_required
@@ -193,7 +207,8 @@ def list_unidad(request):
     user = get_object_or_404(User,email=request.user.email)
     token = str(AccessToken.for_user(user))
     unidades = UnidadMedida.objects.all()
-    context = {"unidades":unidades,'token':token}
+    grupos_usuario = list(request.user.groups.values_list('name', flat=True))
+    context = {"unidades":unidades,'token':token,'grupos_usuario': grupos_usuario}
     return render(request,"html/app/list-unidad.html",context)
 
 @login_required
@@ -204,6 +219,7 @@ def add_entradas(request,entradas_id=None):
     estructura = arbol()
     proveedores = Proveedor.objects.all()
     insumos = Insumo.objects.all()
+    grupos_usuario = list(request.user.groups.values_list('name', flat=True))
 
     if entradas_id:
         entradas_instance = get_object_or_404(Entrada,pk=entradas_id)
@@ -234,6 +250,7 @@ def add_entradas(request,entradas_id=None):
             'insumo_preseleccionado':entradas_instance.insumo.pk,
             'insumos':insumos
             ,'token':token
+            ,'grupos_usuario': grupos_usuario
             }
         return render(request,"html/app/add-entradas.html",context)
     else:
@@ -244,10 +261,11 @@ def list_entradas(request):
     user = get_object_or_404(User,email=request.user.email)
     token = str(AccessToken.for_user(user))
     entradas_instance = Entrada.objects.all()
+    grupos_usuario = list(request.user.groups.values_list('name', flat=True))
     for entrada in entradas_instance:
         entrada.total = entrada.valor_unitario_entrada_a * entrada.cantidad
         entrada.unidad_medida = entrada.insumo.unidad_medida
-    return render(request,"html/app/list-entradas.html",{'entradas':entradas_instance,'token':token})
+    return render(request,"html/app/list-entradas.html",{'entradas':entradas_instance,'token':token,'grupos_usuario': grupos_usuario})
 
 
 @login_required
@@ -256,6 +274,7 @@ def list_entradas_primer_status(request):
     token = str(AccessToken.for_user(user))
     historial = Entrada.history.all().order_by('history_date')
     unidades = UnidadMedida.objects.all()
+    grupos_usuario = list(request.user.groups.values_list('name', flat=True))
     # Crear un diccionario para almacenar el primer registro de cada grupo por el campo identificador (ID)
     primeros_registros = {}
     for entrada in historial:
@@ -267,7 +286,7 @@ def list_entradas_primer_status(request):
 
     # Ordenar los registros por fecha de historial en orden ascendente
     historial_ordenado = sorted(primeros_registros.values(), key=lambda entrada: entrada.history_date)
-    context = {'unidades_medida':unidades,'entradas':historial_ordenado}
+    context = {'unidades_medida':unidades,'entradas':historial_ordenado,'grupos_usuario': grupos_usuario}
     return render(request,"html/app/list-entradas-inicial.html",context)
 
 
@@ -276,24 +295,27 @@ def list_fincas(request):
     user = get_object_or_404(User,email=request.user.email)
     token = str(AccessToken.for_user(user))
     fincas = Finca.objects.all()
-    return render(request,"html/app/list-fincas.html",{'fincas':fincas,'token':token})
+    grupos_usuario = list(request.user.groups.values_list('name', flat=True))
+    return render(request,"html/app/list-fincas.html",{'fincas':fincas,'token':token,'grupos_usuario': grupos_usuario})
 
 @login_required
 def add_fincas(request,fincas_id=None):
     user = get_object_or_404(User,email=request.user.email)
     token = str(AccessToken.for_user(user))
+    grupos_usuario = list(request.user.groups.values_list('name', flat=True))
     if fincas_id:
         fincas_instance = get_object_or_404(Finca,pk=fincas_id)
-        return render(request,"html/app/add-fincas.html",{'fincas_id': fincas_instance.pk,'finca':fincas_instance,'token':token})
+        return render(request,"html/app/add-fincas.html",{'fincas_id': fincas_instance.pk,'finca':fincas_instance,'token':token,'grupos_usuario': grupos_usuario})
     else:
-        return render(request,"html/app/add-fincas.html",{'token':token})
+        return render(request,"html/app/add-fincas.html",{'token':token,'grupos_usuario': grupos_usuario})
 
 @login_required
 def list_lotes(request):
     user = get_object_or_404(User,email=request.user.email)
     token = str(AccessToken.for_user(user))
     lotes = Lotes.objects.all()
-    return render(request,"html/app/list-lotes.html",{'lotes':lotes,'token':token})
+    grupos_usuario = list(request.user.groups.values_list('name', flat=True))
+    return render(request,"html/app/list-lotes.html",{'lotes':lotes,'token':token,'grupos_usuario': grupos_usuario})
 
 @login_required
 def add_lotes(request,lotes_id=None):
@@ -301,7 +323,8 @@ def add_lotes(request,lotes_id=None):
     token = str(AccessToken.for_user(user))
     fincas = Finca.objects.all()
     estructura = arbol()
-    context = {'fincas':fincas,'estructura':estructura,'token':token}
+    grupos_usuario = list(request.user.groups.values_list('name', flat=True))
+    context = {'fincas':fincas,'estructura':estructura,'token':token,'grupos_usuario': grupos_usuario}
 
     if lotes_id:
         lotes_instance = get_object_or_404(Lotes,pk=lotes_id)
@@ -316,7 +339,8 @@ def list_bodegas(request):
     user = get_object_or_404(User,email=request.user.email)
     token = str(AccessToken.for_user(user))
     bodegas = Bodegas.objects.all()
-    return render(request,"html/app/list-bodegas.html",{'bodegas':bodegas,'token':token})
+    grupos_usuario = list(request.user.groups.values_list('name', flat=True))
+    return render(request,"html/app/list-bodegas.html",{'bodegas':bodegas,'token':token,'grupos_usuario': grupos_usuario})
 
 @login_required
 def add_bodegas(request,bodegas_id=None):
@@ -325,7 +349,8 @@ def add_bodegas(request,bodegas_id=None):
     bodegas = Bodegas.objects.all()
     usuarios = User.objects.all()
     estructura = arbol()
-    context = {'bodegas':bodegas,'estructura': estructura,'usuarios':usuarios,'token':token}
+    grupos_usuario = list(request.user.groups.values_list('name', flat=True))
+    context = {'bodegas':bodegas,'estructura': estructura,'usuarios':usuarios,'token':token,'grupos_usuario': grupos_usuario}
 
     if bodegas_id:
         bodega_instance = get_object_or_404(Bodegas,pk=bodegas_id)
@@ -343,17 +368,20 @@ def list_salidas(request):
     user = get_object_or_404(User,email=request.user.email)
     token = str(AccessToken.for_user(user))
     salidas_instance = Salida.objects.all()
-    return render(request,"html/app/list-salidas.html",{'salidas':salidas_instance,'token':token})
+    grupos_usuario = list(request.user.groups.values_list('name', flat=True))
+    return render(request,"html/app/list-salidas.html",{'salidas':salidas_instance,'token':token,'grupos_usuario': grupos_usuario})
 @login_required
 def add_salidas(request, salidas_id=None):
     user = get_object_or_404(User,email=request.user.email)
     token = str(AccessToken.for_user(user))
     insumos = Insumo.objects.all()
     bodegas = Bodegas.objects.all()
+    grupos_usuario = list(request.user.groups.values_list('name', flat=True))
     context = {
         "insumos":insumos,
         "bodegas":bodegas
         ,'token':token
+        ,'grupos_usuario': grupos_usuario
     }
 
 
